da50012843ed748dca58ddba5d346185c367ab9e
abelalv <aalvarezb@unal.edu.co>
2024-06-11 17:45:06 -0500

    otro conficto

--- libreria.py
+++ libreria.py
@@ -51,3 +51,4 @@ def evaluate_inequality(a):
 
 # tengo dos cambios necesarios para el oj conflicto desde la web
 # tengo dos cambios necesarios para el ojo note que tengo algun commit voy a generar otro conflicto
+# tengo dos cambios necesarios para el ojo note que tengo algun commit ottop conflito desde la web
