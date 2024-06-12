6620f814f8ddeb73f0ff67073f0285f84730bc85
abelalv <aalvarezb@unal.edu.co>
2024-06-11 22:38:20 -0500

    solution

--- libreria.py
+++ libreria.py
@@ -54,3 +54,5 @@ def evaluate_inequality(a):
 # tengo dos cambios necesarios para el ojo note que tengo algun commit ottop conflito desde la web
 import numpy as np
 import matplotlib.pyplot as plt
+
+tabine autocmpletado
